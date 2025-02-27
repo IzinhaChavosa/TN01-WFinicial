24c6c30501b2793ffa26e23e67f11e7bc93a9986
izabella.ssantos5 <izabella.ssantos5@L130627979W10-1>
2025-02-26 22:01:05 -0300

    Adicionar arquivos de projeto.

---
+++ CRevisãoAula/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CRevisãoAula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 1° nota do aluno(a)");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite 2° nota do aluno(a)");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if (media < 5)
+            { 
+            Console.WriteLine("Reprovado");
+            }
+            else
+            {
+                Console.WriteLine("Recuperação");
+            }
+
+        }
+    }
+}
