400604a4e3dc135d95054ec9619305c7eb3b2e5c
IZABELLA SILVA SANTOS <izabella.ssantos5@senacsp.edu.br>
2025-02-28 19:10:35 -0300

    feat exercício 3 corrigido no Windows Form

--- WFinicial/FormEx3.Designer.cs
+++ WFinicial/FormEx3.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormEx3";
+            txtNum1 = new TextBox();
+            lblNum1 = new Label();
+            btnMaior = new Button();
+            lblNum2 = new Label();
+            txtNum2 = new TextBox();
+            SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(101, 59);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(100, 23);
+            txtNum1.TabIndex = 0;
+            // 
+            // lblNum1
+            // 
+            lblNum1.AutoSize = true;
+            lblNum1.Location = new Point(29, 67);
+            lblNum1.Name = "lblNum1";
+            lblNum1.Size = new Size(60, 15);
+            lblNum1.TabIndex = 1;
+            lblNum1.Text = "Número 1";
+            // 
+            // btnMaior
+            // 
+            btnMaior.Location = new Point(108, 118);
+            btnMaior.Name = "btnMaior";
+            btnMaior.Size = new Size(93, 23);
+            btnMaior.TabIndex = 2;
+            btnMaior.Text = "Mostrar Maior";
+            btnMaior.UseVisualStyleBackColor = true;
+            btnMaior.Click += btnMaior_Click;
+            // 
+            // lblNum2
+            // 
+            lblNum2.AutoSize = true;
+            lblNum2.Location = new Point(29, 97);
+            lblNum2.Name = "lblNum2";
+            lblNum2.Size = new Size(60, 15);
+            lblNum2.TabIndex = 3;
+            lblNum2.Text = "Número 2";
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(101, 89);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(100, 23);
+            txtNum2.TabIndex = 4;
+            // 
+            // FormEx3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtNum2);
+            Controls.Add(lblNum2);
+            Controls.Add(btnMaior);
+            Controls.Add(lblNum1);
+            Controls.Add(txtNum1);
+            Name = "FormEx3";
+            Tag = "FormEx3";
+            Text = "FormEx3";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtNum1;
+        private Label lblNum1;
+        private Button btnMaior;
+        private Label lblNum2;
+        private TextBox txtNum2;
     }
 }
--- WFinicial/FormEx3.cs
+++ WFinicial/FormEx3.cs
@@ -11,10 +11,24 @@ using System.Windows.Forms;
 namespace WFinicial
 {
     public partial class FormEx3 : Form
-    {
+    { 
+
         public FormEx3()
         {
             InitializeComponent();
+        }
+
+        private void btnMaior_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+
+            int maior = Math.Max(num1, num2);
+
+            string msg = $"O maior número é: " + maior;
+
+            MessageBox.Show(msg,"info", MessageBoxButtons.OK,MessageBoxIcon.Information);
+
         }
     }
 }
