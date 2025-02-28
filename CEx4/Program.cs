edccda29d62bf93cb401fd6fb3dc09c8c30ecaba
IZABELLA SILVA SANTOS <izabella.ssantos5@senacsp.edu.br>
2025-02-28 19:19:37 -0300

    feat exercício 4 corrigido no console

---
+++ CEx4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());Console.WriteLine("Digite o segundo número: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());//outra forma de converter
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+            Console.WriteLine($"\nO Número maior é {maior} é maior");
+        }
+    }
+}
