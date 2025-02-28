6daea377e6d7e2544e2af6deeab9b7c2151bbfed
IZABELLA SILVA SANTOS <izabella.ssantos5@senacsp.edu.br>
2025-02-28 19:45:47 -0300

    fix Exercício 4 Corrigido no console

---
+++ CEx5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CEx5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.WriteLine("Digite o segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine()); Console.WriteLine("Digite o segundo número: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());//outra forma de converter
+
+            int maior = Math.Max(num1, num2);
+            maior = Math.Max(maior, num3);
+            int menor = Math.Min(num1, num2);
+            menor = Math.Min(maior, num3);
+            Console.WriteLine($"\nO Número {maior} é o maior valor");
+            Console.WriteLine($"\nO Número {menor} é o menor valor");
+        }
+    }
+}
