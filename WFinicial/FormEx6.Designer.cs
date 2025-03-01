96323eb4842aae831fecf2eeadb840d5312a90a7
IZABELLA SILVA SANTOS <izabella.ssantos5@senacsp.edu.br>
2025-02-28 21:15:00 -0300

    feat: Exercício 6 Corrigido no Windows Forms

--- WFinicial/FormEx6.Designer.cs
+++ WFinicial/FormEx6.Designer.cs
@@ -28,18 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnResultado = new Button();
+            lblNome = new Label();
+            lblValorHora = new Label();
+            txtNome = new TextBox();
+            txtValorHora = new TextBox();
+            lblquantHoras = new Label();
+            txtQuantHoras = new TextBox();
             SuspendLayout();
+            // 
+            // btnResultado
+            // 
+            btnResultado.Location = new Point(107, 114);
+            btnResultado.Name = "btnResultado";
+            btnResultado.Size = new Size(75, 23);
+            btnResultado.TabIndex = 0;
+            btnResultado.Text = "Resultado";
+            btnResultado.UseVisualStyleBackColor = true;
+            btnResultado.Click += btnResultado_Click;
+            // 
+            // lblNome
+            // 
+            lblNome.AutoSize = true;
+            lblNome.Location = new Point(26, 16);
+            lblNome.Name = "lblNome";
+            lblNome.Size = new Size(40, 15);
+            lblNome.TabIndex = 1;
+            lblNome.Text = "Nome";
+            // 
+            // lblValorHora
+            // 
+            lblValorHora.AutoSize = true;
+            lblValorHora.Location = new Point(26, 50);
+            lblValorHora.Name = "lblValorHora";
+            lblValorHora.Size = new Size(76, 15);
+            lblValorHora.TabIndex = 2;
+            lblValorHora.Text = "Valor da hora";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(168, 13);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(100, 23);
+            txtNome.TabIndex = 3;
+            // 
+            // txtValorHora
+            // 
+            txtValorHora.Location = new Point(168, 42);
+            txtValorHora.Name = "txtValorHora";
+            txtValorHora.Size = new Size(100, 23);
+            txtValorHora.TabIndex = 4;
+            // 
+            // lblquantHoras
+            // 
+            lblquantHoras.AutoSize = true;
+            lblquantHoras.Location = new Point(26, 82);
+            lblquantHoras.Name = "lblquantHoras";
+            lblquantHoras.Size = new Size(120, 15);
+            lblquantHoras.TabIndex = 5;
+            lblquantHoras.Text = "Quantidade De Horas";
+            // 
+            // txtQuantHoras
+            // 
+            txtQuantHoras.Location = new Point(168, 71);
+            txtQuantHoras.Name = "txtQuantHoras";
+            txtQuantHoras.Size = new Size(100, 23);
+            txtQuantHoras.TabIndex = 6;
             // 
             // FormEx6
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(437, 274);
+            Controls.Add(txtQuantHoras);
+            Controls.Add(lblquantHoras);
+            Controls.Add(txtValorHora);
+            Controls.Add(txtNome);
+            Controls.Add(lblValorHora);
+            Controls.Add(lblNome);
+            Controls.Add(btnResultado);
             Name = "FormEx6";
             Text = "FormEx6";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnResultado;
+        private Label lblNome;
+        private Label lblValorHora;
+        private TextBox txtNome;
+        private TextBox txtValorHora;
+        private Label lblquantHoras;
+        private TextBox txtQuantHoras;
     }
 }
